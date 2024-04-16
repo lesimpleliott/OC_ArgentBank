6f75eda1c87be6b8c364e795c40998fd7d98004e
Eliott Lesimple <lesimpleliott@gmail.com>
2024-04-16 16:49:12 +0200

    colors and data update

--- src/assets/styles/color.jsx
+++ src/assets/styles/color.jsx
@@ -1,7 +1,8 @@
 export const color = {
-    main: '#42b983',
-    second: '#00bc77',
-    background: '#12002b',
-    text: '#2c3e50',
-    border: '#ccc',
-}
+  main: "#42b983",
+  second: "#00bc77",
+  background: "#12002b",
+  text: "#2c3e50",
+  border: "#ccc",
+  error: "#ff0000",
+};
